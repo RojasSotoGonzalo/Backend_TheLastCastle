ad6cb1837eae1202d61b72f59aef6cf4dcb684dd
Andrew <gonzalo.rojas@ucb.edu.bo>
2023-11-13 23:21:36 -0400

    Primera Etapa Backend

---
+++ src/test/java/com/the_last_castle/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_last_castle.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
